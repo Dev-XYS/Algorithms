2fd8e5b8df1062451d822d5c2670aa69bbaf36c0
Dev-XYS <xiaoyushuo0419@live.com>
2016-09-11 10:01:37 +0800

    增加了区间修改的功能，合并单点修改与区间修改，更改了区间信息的存储方式

--- Segment-Tree(Maximum).cpp
+++ Segment-Tree(Maximum).cpp
@@ -4,7 +4,13 @@
 
 using namespace std;
 
-int d[ELEMENT_COUNT], T[ELEMENT_COUNT * 4], lazy[ELEMENT_COUNT * 4];
+struct node
+{
+	int l, r, max, lazy;
+}T[ELEMENT_COUNT * 4];
+
+int n;
+int d[ELEMENT_COUNT];
 
 inline int max(int x, int y)
 {
@@ -13,65 +19,75 @@ inline int max(int x, int y)
 
 void build_tree(int l, int r, int k)
 {
+	T[k].l = l;
+	T[k].r = r;
 	if (l == r)
 	{
-		T[k] = d[l];
+		T[k].max = d[l];
 	}
 	else
 	{
 		int mid = (l + r) >> 1;
 		build_tree(l, mid, k << 1);
 		build_tree(mid + 1, r, (k << 1) + 1);
-		T[k] = max(T[k << 1], T[(k << 1) + 1]);
+		T[k].max = max(T[k << 1].max, T[(k << 1) + 1].max);
 	}
 }
 
-int query(int l, int r, int b, int e, int k)
+int get_max(int l, int r, int k)
 {
-	if (l == b && r == e)
+	if (l == T[k].l && r == T[k].r)
 	{
-		return T[k];
-	}
-	
-	int mid = (l + r) >> 1;
-	if (b > mid)
-	{
-		return query(mid + 1, r, b, e, (k << 1) + 1);
-	}
-	else if (e <= mid)
-	{
-		return query(l, mid, b, e, k << 1);
+		return T[k].max + T[k].lazy;
 	}
 	else
 	{
-		return max(query(l, mid, b, mid, k << 1), query(mid + 1, r, mid + 1, e, (k << 1) + 1));
-	}
-}
-
-void update_one(int l, int r, int v, int x, int k)
-{
-	if (l == r)
-	{
-		T[k] = x;
-	}
-	else
-	{
-		int mid = (l + r) >> 1;
-		if (v <= mid)
+		T[k].max += T[k].lazy;
+		T[k << 1].lazy += T[k].lazy;
+		T[(k << 1) + 1].lazy += T[k].lazy;
+		T[k].lazy = 0;
+		if (r <= T[k << 1].r)
 		{
-			update_one(l, mid, v, x, k << 1);
+			return get_max(l, r, k << 1);
+		}
+		else if (l >= T[(k << 1) + 1].l)
+		{
+			return get_max(l, r, (k << 1) + 1);
 		}
 		else
 		{
-			update_one(mid + 1, r, v, x, (k << 1) + 1);
+			return max(get_max(l, T[k << 1].r, k << 1), get_max(T[(k << 1) + 1].l, r, (k << 1) + 1));
 		}
-		T[k] = max(T[k << 1], T[(k << 1) + 1]);
+	}
+}
+
+int delta;
+void update_segment(int l, int r, int k)
+{
+	if (l == T[k].l && r == T[k].r)
+	{
+		T[k].lazy += delta;
+	}
+	else
+	{
+		if (r <= T[k << 1].r)
+		{
+			update_segment(l, r, k << 1);
+		}
+		else if (l >= T[(k << 1) + 1].l)
+		{
+			update_segment(l, r, (k << 1) + 1);
+		}
+		else
+		{
+			update_segment(l, T[k << 1].r, k << 1);
+			update_segment(T[(k << 1) + 1].l, r, (k << 1) + 1);
+		}
 	}
 }
 
 int main()
 {
-	int n;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 	{
@@ -80,20 +96,34 @@ int main()
 	build_tree(0, n - 1, 1);
 	while (true)
 	{
-		int o, a, b;
+		int o, a, b, d;
 		scanf("%d", &o);
 		switch (o)
 		{
 		case 1:
 			scanf("%d%d", &a, &b);
-			printf("%d\n", query(0, n - 1, a - 1, b - 1, 1));
+			printf("%d\n", get_max(a - 1, b - 1, 1));
 			break;
 		case 2:
-			scanf("%d%d", &a, &b);
-			update_one(0, n - 1, a - 1, b, 1);
+			scanf("%d%d%d", &a, &b, &d);
+			delta = d;
+			update_segment(a - 1, b - 1, 1);
 			break;
+		case 3:
+			for (int i = 0; i < n * 2; i++)
+			{
+				printf("%2d ", i);
+			}
+			printf("\n");
+			for (int i = 0; i < n * 2; i++)
+			{
+				printf("%2d ", T[i].lazy);
+			}
+			printf("\n");
+			break;
+		default:
+			printf("Instruction incorrect!\n");
 		}
 	}
-	
 	return 0;
 }
