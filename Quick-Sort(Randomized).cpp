2313ed6496f473b103acbae2b2340174b714e26d
Dev-XYS <xiaoyushuo0419@live.com>
2016-10-05 10:20:48 +0800

    快速排序的随机化版本

---
+++ Quick-Sort(Randomized).cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstdlib>
+
+#define ELEMENT_COUNT 1000000 
+
+using namespace std;
+
+int d[ELEMENT_COUNT];
+
+int flag = 0; 
+
+void qsort(int l, int r)
+{
+	if (l < r)
+	{
+		int _rand = rand() % (r - l + 1) + l;
+		
+		int temp = d[_rand];
+		d[_rand] = d[r];
+		d[r] = temp;
+		
+		int x = d[r];
+		int j = l - 1;
+		
+		for (int i = l; i <= r; i++)
+		{
+			if (d[i] <= x)
+			{
+				j++;
+				temp = d[i];
+				d[i] = d[j];
+				d[j] = temp;
+			}
+		}
+		
+		qsort(l, j - 1);
+		qsort(j + 1, r);
+	}
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &d[i]);
+	}
+	
+	qsort(0, n - 1);
+	
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", d[i]);
+	}
+	
+	return 0;
+}
