940f573ff0fa9ebb9555805b3b8ddc8f2a48cbfb
Dev-XYS <xiaoyushuo0419@live.com>
2016-12-05 22:00:31 +0800

    基本的复数类

---
+++ Complex-Number.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+
+using namespace std;
+
+struct complex
+{
+	double re, im;
+	complex(double _re = 0, double _im = 0) : re(_re), im(_im) { }
+};
+
+complex operator + (const complex &x, const complex &y)
+{
+	return complex(x.re + y.re, x.im + y.im);
+}
+
+complex operator - (const complex &x, const complex &y)
+{
+	return complex(x.re - y.re, x.im - y.im);
+}
+
+complex operator * (const complex &x, const complex &y)
+{
+	return complex(x.re * y.re - x.im * y.im, x.re * y.re + x.im * y.im);
+}
+
+complex operator / (const complex &x, const complex &y)
+{
+	return complex((x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im), (x.im * y.re - x.re * y.im) / (y.re * y.re + y.im * y.im));
+}
+
+int main()
+{
+	complex x, y, res;
+	char op;
+	while (true)
+	{
+		scanf("%lf%lf %c %lf%lf", &x.re, &x.im, &op, &y.re, &y.im);
+		switch (op)
+		{
+		case '+':
+			res = x + y;
+			break;
+		case '-':
+			res = x - y;
+			break;
+		case '*':
+			res = x * y;
+			break;
+		case '/':
+			res = x / y;
+			break;
+		default:
+			continue;
+		}
+		printf("(%.2lf + (%.2lf)i) %c (%.2lf + (%.2lf)i) = %.2lf + (%.2lf)i\n", x.re, x.im, op, y.re, y.im, res.re, res.im);
+	}
+	return 0;
+}
