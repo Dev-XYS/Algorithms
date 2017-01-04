a4ab8e13c16b386c7176ca0acfaa41db37c8b0e4
Dev-XYS <xiaoyushuo0419@live.com>
2017-01-04 23:20:57 +0800

    添加例题：BZOJ - 1036 树的统计

--- Heavy-Light-Decomposition.cpp
+++ Heavy-Light-Decomposition.cpp
@@ -1,53 +1,107 @@
 #include <cstdio>
+#include <algorithm>
 
+#define NIL 0
+#define INF 1000000000
 #define VERTEX_COUNT 100000
 #define EDGE_COUNT 200000
+#define POOL_SIZE 200000
 
 using namespace std;
 
+struct edge;
 struct vertex
 {
-	int first, depth, fa, fae, hson, size, dfn, top;
-}V[VERTEX_COUNT];
+	edge *first;
+	int w, depth, size, ord;
+	vertex *p, *heavy, *top;
+}*nil, V[VERTEX_COUNT];
 
 struct edge
 {
-	int endp, next, w;
+	edge *next;
+	vertex *endp;
+	edge(edge *_next = NIL, vertex *_endp = nil);
+	void* operator new (size_t);
 }E[EDGE_COUNT];
 
-struct segnode
-{
-	segnode *lch, *rch;
-	int sum, lazy;
-	void* operator new (size_t size);
-}segpool[VERTEX_COUNT * 4];
-int segtop = 0;
+edge::edge(edge *_next, vertex *_endp) : next(_next), endp(_endp) { }
 
-struct segment_tree
+int Etop = 0;
+void* edge::operator new (size_t)
 {
-	segnode *root;
-	int begin, end;
-	
-	void build(segnode *&u, int l, int r);
-	void build();
-	void modify(segnode *u, int ul, int ur, int il, int ir, int delta);
-	int query(segnode *u, int ul, int ur, int il, int ir);
-	int query(int il, int ir);
-}seg;
-
-int ivc, ec = 1, dfn = 1, map[VERTEX_COUNT];
-
-void* segnode::operator new (size_t size)
-{
-	return segpool + segtop++;
+	return E + Etop++;
 }
 
-void segment_tree::build(segnode *&u, int l, int r)
+struct node
 {
-	u = new segnode;
+	node *lch, *rch;
+	int sum, max;
+	node();
+	void* operator new (size_t);
+}*root, pool[POOL_SIZE];
+
+node::node() : lch(NIL), rch(NIL), sum(0), max(0) { }
+
+int pooltop = 0;
+void* node::operator new (size_t)
+{
+	return pool + pooltop++;
+}
+
+int ivc, m, ord = 0;
+vertex *map[VERTEX_COUNT];
+
+void add_edge(vertex *u, vertex *v)
+{
+	u->first = new edge(u->first, v);
+}
+
+void DFS1(vertex *u, vertex *p, int depth)
+{
+	vertex *heavy = nil;
+	u->p = p;
+	u->depth = depth;
+	u->size = 1;
+	for (edge *cur = u->first; cur != NIL; cur = cur->next)
+	{
+		if (cur->endp != p)
+		{
+			DFS1(cur->endp, u, depth + 1);
+			u->size += cur->endp->size;
+			if (cur->endp->size > heavy->size)
+			{
+				heavy = cur->endp;
+			}
+		}
+	}
+	u->heavy = heavy;
+}
+
+void DFS2(vertex *u, vertex *top)
+{
+	u->top = top;
+	u->ord = ++ord;
+	map[ord] = u;
+	if (u->heavy != nil)
+	{
+		DFS2(u->heavy, top);
+	}
+	for (edge *cur = u->first; cur != NIL; cur = cur->next)
+	{
+		if (cur->endp != u->heavy && cur->endp != u->p)
+		{
+			DFS2(cur->endp, cur->endp);
+		}
+	}
+}
+
+void build(node *&u, int l, int r)
+{
+	u = new node;
 	if (l == r)
 	{
-		u->sum = V[map[l]].fae;
+		u->sum = u->max = map[l]->w;
 	}
 	else
 	{
@@ -55,169 +109,177 @@ void segment_tree::build(segnode *&u, int l, int r)
 		build(u->lch, l, mid);
 		build(u->rch, mid + 1, r);
 		u->sum = u->lch->sum + u->rch->sum;
+		u->max = max(u->lch->max, u->rch->max);
 	}
 }
 
-void segment_tree::build()
+void modify(node *u, int l, int r, int k, int x)
 {
-	build(root, begin, end);
-}
-
-void segment_tree::modify(segnode *u, int ul, int ur, int il, int ir, int delta)
-{
-	if (ul == il && ur == ir)
+	if (l == r)
 	{
-		u->sum += delta * (ur - ul + 1);
-		u->lazy += delta;
+		u->sum = u->max = x;
 	}
 	else
 	{
-		int mid = (ul + ur) >> 1;
-		if (ir <= mid)
+		int mid = (l + r) >> 1;
+		if (k <= mid)
 		{
-			modify(u->lch, ul, mid, il, ir, delta);
-		}
-		else if (il > mid)
-		{
-			modify(u->rch, mid + 1, ur, il, ir, delta);
+			modify(u->lch, l, mid, k, x);
 		}
 		else
 		{
-			modify(u->lch, ul, mid, il, mid, delta);
-			modify(u->rch, mid + 1, ur, mid + 1, ir, delta);
+			modify(u->rch, mid + 1, r, k, x);
 		}
-		u->sum += u->lch->sum + u->rch->sum;
+		u->sum = u->lch->sum + u->rch->sum;
+		u->max = max(u->lch->max, u->rch->max);
 	}
 }
 
-int segment_tree::query(segnode *u, int ul, int ur, int il, int ir)
+int query_sum(node *u, int l, int r, int ql, int qr)
 {
-	if (ul == il && ur == ir)
+	if (l == ql && r == qr)
 	{
 		return u->sum;
 	}
+	int mid = (l + r) >> 1;
+	if (qr <= mid)
+	{
+		return query_sum(u->lch, l, mid, ql, qr);
+	}
+	else if (ql > mid)
+	{
+		return query_sum(u->rch, mid + 1, r, ql, qr);
+	}
 	else
 	{
-		int mid = (ul + ur) >> 1;
-		u->lch->sum += u->lazy * (mid - ul + 1);
-		u->lch->lazy += u->lazy;
-		u->rch->sum += u->lazy * (ur - mid);
-		u->rch->lazy += u->lazy;
-		u->lazy = 0;
-		if (ir <= mid)
+		return query_sum(u->lch, l, mid, ql, mid) + query_sum(u->rch, mid + 1, r, mid + 1, qr);
+	}
+}
+
+int query_max(node *u, int l, int r, int ql, int qr)
+{
+	if (l == ql && r == qr)
+	{
+		return u->max;
+	}
+	int mid = (l + r) >> 1;
+	if (qr <= mid)
+	{
+		return query_max(u->lch, l, mid, ql, qr);
+	}
+	else if (ql > mid)
+	{
+		return query_max(u->rch, mid + 1, r, ql, qr);
+	}
+	else
+	{
+		return max(query_max(u->lch, l, mid, ql, mid), query_max(u->rch, mid + 1, r, mid + 1, qr));
+	}
+}
+
+void tmodify(vertex *x, int v)
+{
+	modify(root, 1, ivc, x->ord, v);
+}
+
+int tquery_sum(vertex *x, vertex *y)
+{
+	int res = 0;
+	vertex *ux, *uy;
+	while (x->top != y->top)
+	{
+		ux = x->top == x ? x->p : x->top;
+		uy = y->top == y ? y->p : y->top;
+		if (ux->depth < uy->depth)
 		{
-			return query(u->lch, ul, mid, il, ir);
+			swap(x, y);
 		}
-		else if (il > mid)
+		if (x->top == x)
 		{
-			return query(u->rch, mid + 1, ur, il, ir);
+			res += query_sum(root, 1, ivc, x->ord, x->ord);
+			x = x->p;
 		}
 		else
 		{
-			return query(u->lch, ul, mid, il, mid) + query(u->rch, mid + 1, ur, mid + 1, ir);
+			res += query_sum(root, 1, ivc, x->top->ord + 1, x->ord);
+			x = x->top;
 		}
 	}
-}
-
-int segment_tree::query(int il, int ir)
-{
-	return query(root, begin, end, il, ir);
-}
-
-void add_edge(int u, int v, int w)
-{
-	E[ec].next = V[u].first;
-	V[u].first = ec;
-	E[ec].endp = v;
-	E[ec].w = w;
-	ec++;
-}
-
-void DFS1(int u, int depth, int fa, int fae)
-{
-	int hson = 0;
-	V[u].depth = depth;
-	V[u].fa = fa;
-	V[u].fae = fae;
-	V[u].size = 1;
-	for (int cur = V[u].first; cur != 0; cur = E[cur].next)
+	if (x->depth > y->depth)
 	{
-		if (E[cur].endp != fa)
-		{
-			DFS1(E[cur].endp, depth + 1, u, E[cur].w);
-			V[u].size += V[E[cur].endp].size;
-			if (V[E[cur].endp].size > V[hson].size)
-			{
-				hson = E[cur].endp;
-			}
-		}
+		swap(x, y);
 	}
-	V[u].hson = hson;
-}
-
-void DFS2(int u, int fa, int top)
-{
-	V[u].dfn = dfn++;
-	map[V[u].dfn] = u;
-	V[u].top = top;
-	if (V[u].hson != 0)
-	{
-		DFS2(V[u].hson, u, top);
-	}
-	for (int cur = V[u].first; cur != 0; cur = E[cur].next)
-	{
-		if (E[cur].endp != V[u].hson && E[cur].endp != fa)
-		{
-			DFS2(E[cur].endp, u, E[cur].endp);
-		}
-	}
-}
-
-int query(int x, int y)
-{
-	int res = 0;
-	while (V[x].top != V[y].top)
-	{
-		int _x = V[x].top == x ? V[x].fa : V[x].top, _y = V[y].top == y ? V[y].fa : V[y].top;
-		if (V[_x].depth < V[_y].depth)
-		{
-			int temp = x;
-			x = y;
-			y = temp;
-		}
-		res += V[x].top == x ? V[x].fae : seg.query(V[V[x].top].dfn + 1, V[x].dfn);
-		x = V[x].top == x ? V[x].fa : V[x].top;
-	}
-	if (V[x].depth > V[y].depth)
-	{
-		res += seg.query(V[y].dfn, V[x].dfn) - V[y].fae;
-	}
-	else
-	{
-		res += seg.query(V[x].dfn, V[y].dfn) - V[x].fae;
-	}
+	res += query_sum(root, 1, ivc, x->ord, y->ord);
 	return res;
 }
 
+int tquery_max(vertex *x, vertex *y)
+{
+	int res = -INF;
+	vertex *ux, *uy;
+	while (x->top != y->top)
+	{
+		ux = x->top == x ? x->p : x->top;
+		uy = y->top == y ? y->p : y->top;
+		if (ux->depth < uy->depth)
+		{
+			swap(x, y);
+		}
+		if (x->top == x)
+		{
+			res = max(res, query_max(root, 1, ivc, x->ord, x->ord));
+			x = x->p;
+		}
+		else
+		{
+			res = max(res, query_max(root, 1, ivc, x->top->ord + 1, x->ord));
+			x = x->top;
+		}
+	}
+	if (x->depth > y->depth)
+	{
+		swap(x, y);
+	}
+	res = max(res, query_max(root, 1, ivc, x->ord, y->ord));
+	return res;
+}
+		
 int main()
 {
-	int u, v, w;
+	int u, v, a, b;
+	char op[10];
 	scanf("%d", &ivc);
 	for (int i = 1; i < ivc; i++)
 	{
-		scanf("%d%d%d", &u, &v, &w);
-		add_edge(u, v, w);
-		add_edge(v, u, w);
-	}
-	DFS1(1, 1, 0, 0);
-	DFS2(1, 0, 1);
-	seg.begin = 1;
-	seg.end = ivc;
-	seg.build();
-	while (true)
-	{
 		scanf("%d%d", &u, &v);
-		printf("%d\n", query(u, v));
+		add_edge(V + u, V + v);
+		add_edge(V + v, V + u);
+	}
+	for (int i = 1; i <= ivc; i++)
+	{
+		scanf("%d", &(V + i)->w);
+	}
+	nil = new vertex;
+	nil->size = 0;
+	DFS1(V + 1, nil, 1);
+	DFS2(V + 1, V + 1);
+	build(root, 1, ivc);
+	scanf("%d", &m);
+	while (m--)
+	{
+		scanf("%s%d%d", op, &a, &b);
+		switch (op[1])
+		{
+		case 'H':
+			tmodify(V + a, b);
+			break;
+		case 'S':
+			printf("%d\n", tquery_sum(V + a, V + b));
+			break;
+		case 'M':
+			printf("%d\n", tquery_max(V + a, V + b));
+			break;
+		}
 	}
 	return 0;
 }
